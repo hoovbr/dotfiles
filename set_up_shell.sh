6cdffa745ceac386a4d90c291fca00af39a4b67a
Roger Oba <rogerluan.oba@gmail.com>
2023-12-14 15:56:39 -0300

    Add troubleshooting instructions.

--- set_up_shell.sh
+++ set_up_shell.sh
@@ -3,6 +3,27 @@
 # This script installs QoL tools to our shell, such as zsh, oh-my-zsh, powerline-shell and more.
 # Precondition: DOTFILES_DIR must be set and exist.
 
+function print_change_shell_troubleshooting() {
+  echo "❗️ Looks like you had issues running chsh to change the default shell to zsh."
+  echo "Try running the following command manually:"
+  echo
+  echo "sudo chsh -s $(which zsh) $(whoami)"
+  echo
+  echo "If it still doesn't work, try running the following command:"
+  echo
+  echo "sudo nano /etc/default/useradd"
+  echo
+  echo "And edit the line that says 'SHELL=/bin/sh' to say 'SHELL=$(which zsh)' instead."
+  echo "This will ensure all the new users created will have zsh as their default shell."
+  echo "To change the default shell of the existing users, run the following command:"
+  echo
+  echo "sudo nano /etc/passwd"
+  echo
+  echo "And find the users you want to edit this setting, and modify the last component of their line to say $(which zsh) instead of /bin/sh."
+  echo
+  echo "I hope this helps! 👋"
+}
+
 # Update packages first
 package_manager update -y || true # Ignore failures
 
@@ -15,7 +36,7 @@ if ! command -v chsh >/dev/null 2>&1; then
 fi
 
 # Set the default shell to zsh
-sudo chsh -s $(which zsh) $(whoami)
+sudo chsh -s $(which zsh) $(whoami) || print_change_shell_troubleshooting
 
 # Install oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
