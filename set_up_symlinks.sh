705f06ce94e90ed0c075a3c7d58c9e5499261a87
Roger Oba <rogerluan.oba@gmail.com>
2024-09-12 11:27:55 -0300

    Add symlinks for the powerline-shell config.

--- set_up_symlinks.sh
+++ set_up_symlinks.sh
@@ -13,3 +13,6 @@ ln -sf $DOTFILES_DIR/.gemrc $HOME/.gemrc
 ln -sf $DOTFILES_DIR/.paths $HOME/.paths
 ln -sf $DOTFILES_DIR/.ruby-version $HOME/.ruby-version
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+
+mkdir -p $HOME/.config/powerline-shell
+ln -sf $DOTFILES_DIR/powerline-shell-config.json $HOME/.config/powerline-shell/config.json
